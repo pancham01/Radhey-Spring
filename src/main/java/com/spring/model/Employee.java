ec667a0c2e49d079ba50cdb796a3073b46dd522c
pancham01 <pancham9557@gmail.com>
2025-07-19 12:00:59 +0530

    update

--- src/main/java/com/spring/model/Employee.java
+++ src/main/java/com/spring/model/Employee.java
@@ -1,21 +1,56 @@
 package com.spring.model;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class Employee {
 	private int id;
 	private String name, gender;
 	private Address address;
-	
+
+	private List<Integer> list;
+	private Set<Integer> set;
+	private Map<Integer, String> map;
+
 	public Employee() {
 		super();
-		System.out.println("Employee.Employee(0)");
 	}
-	
-	public Employee(int id, String name, String gender, Address address) {
+
+	public Employee(int id, String name, String gender, Address address, List<Integer> list, Set<Integer> set,
+			Map<Integer, String> map) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.gender = gender;
 		this.address = address;
+		this.list = list;
+		this.set = set;
+		this.map = map;
+	}
+
+	public List<Integer> getList() {
+		return list;
+	}
+
+	public void setList(List<Integer> list) {
+		this.list = list;
+	}
+
+	public Set<Integer> getSet() {
+		return set;
+	}
+
+	public void setSet(Set<Integer> set) {
+		this.set = set;
+	}
+
+	public Map<Integer, String> getMap() {
+		return map;
+	}
+
+	public void setMap(Map<Integer, String> map) {
+		this.map = map;
 	}
 
 	public Address getAddress() {
@@ -24,37 +59,37 @@ public class Employee {
 
 	public void setAddress(Address address) {
 		this.address = address;
-		System.out.println("Employee.Address()");
 
 	}
 
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
-		System.out.println("Employee.setId()");
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
-		System.out.println("Employee.setName()");
 	}
+
 	public String getGender() {
 		return gender;
 	}
+
 	public void setGender(String gender) {
 		this.gender = gender;
-		System.out.println("Employee.setGender()");
 	}
 
 	@Override
 	public String toString() {
-		return "Employee [id=" + id + ", name=" + name + ", gender=" + gender + ", address=" + address + "]";
+		return "Employee [id=" + id + ", name=" + name + ", gender=" + gender + ", address=" + address + ", list="
+				+ list + ", set=" + set + ", map=" + map + "]";
 	}
-
-
 
 }
